29e944b87227b5f44a5bd4c4be1543c6d5370ee3
edwinsanch33 <edwinsanch33@gmail.com>
2020-03-27 19:38:29 -0400

    update states of inputs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    shape: '',
+    color: '',
+    eyeSize: '',
+    eyeColor: '',
+    mouthSize: '',
+    mouthColor: '',
+    width: '',
+    height: '',
+  }
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+  render() {
+    const { shape, color, eyeColor, eyeSize, mouthSize, mouthColor, height, width } = this.state
+    const { handleChange } = this
+    let eyes = { 
+      transform: `scale(${eyeSize})`,
+      background: `${eyeColor}` 
+    }
+    return (
+      <Fragment>
+        <main>
+          <form>
+            <div>
+              <label htmlFor="shape">Shape</label>
+              <input type="range" name="shape" min="0" max="50" onChange={handleChange}/>
+            </div>
+            <div>
+              <label htmlFor="color">Color</label>
+              <input type="color" name="color" onChange={handleChange} value={color}/>
+            </div>
+            <div>
+              <label htmlFor="height">Height</label>
+              <input type="range" name="height" min="100" max="400" onChange={handleChange}/>
+            </div>
+            <div>
+              <label htmlFor="width">Width</label>
+              <input type="range" name="width" min="100" max="400" onChange={handleChange}/>
+            </div>
+            <div>
+              <label htmlFor="eyeSize">Eye Size</label>
+              <input type="range" name="eyeSize" min="0.2" max="2" step="0.2" onChange={handleChange}/>
+            </div>
+            <div>
+              <label htmlFor="eyeColor">Eye Color</label>
+              <input type="color" name="eyeColor" onChange={handleChange} value={eyeColor}/>
+            </div>
+            <div>
+              <label htmlFor="eyeColor">Mouth Size</label>
+              <input type="range" name="mouthSize" min="0.5" max="1.5" step="0.1" onChange={handleChange}/>
+            </div>
+            <div>
+              <label htmlFor="eyeColor">Mouth Color</label>
+              <input type="color" name="mouthColor" onChange={handleChange} value={mouthColor}/>
+            </div>
+          </form>
+          <section>
+            <div  
+              style={{ 
+                borderRadius: `${shape}%`, 
+                background: `${color}`, 
+                height: `${height}px`, 
+                width: `${width}px` 
+              }} 
+              className="smiley"
+            >
+              <div className="eyes">
+                <div 
+                  style={eyes} 
+                  className="eye"
+                ></div>
+                <div 
+                  style={eyes}
+                  className="eye"
+                ></div>
+              </div>
+              <div 
+                style={{ transform: `scale(${mouthSize})`, background: `${mouthColor}`}}
+                className="mouth"
+              ></div>
+            </div>
+          </section>
+        </main>
+      </Fragment>
+    )
+  }
 }
-
-export default App;
